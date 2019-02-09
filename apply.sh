2620a3498ea7211f5b8de1f049e3ee5e2f96bdd2
Yan Mingzhi <yanmingzhi@elastos.org>
2019-02-09 13:38:26 +0800

    add apply shell file

---
+++ apply.sh
@@ -0,0 +1,11 @@
+cp bash_profile ~/.bash_profile
+cp zshrc ~/.zshrc
+cp envrc ~/.envrc
+
+cp vimrc ~/.vimrc
+cp ideavimrc ~/.ideavimrc
+
+cp tmux.conf ~/.tmux.conf
+cp alacritty.yml ~/.config/alacritty/alacritty.yml
+
+cp gitconfig ~/.gitconfig
