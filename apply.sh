95c60f5046f3e4f26cf82ddbfc7009d379d5f713
Yan Mingzhi <realraindust@gmail.com>
2020-07-27 13:46:39 +0800

    add lint version for linux

--- apply.sh
+++ apply.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
 cp bash_profile ~/.bash_profile
-cp zshrc ~/.zshrc
 cp envrc ~/.envrc
 cp aliases ~/.aliases
-cp oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
 cp config.fish ~/.config/fish/config.fish
 cp -r fish ~/.config/fish/
 
 cp init.vim ~/.config/nvim/init.vim
-cp ideavimrc ~/.ideavimrc
-cp keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-cp settings.json ~/Library/Application\ Support/Code/User/settings.json
-cp preferences.xiconfig ~/Library/Application\ Support/XiEditor/preferences.xiconfig
 
 cp tmux.conf ~/.tmux.conf
-cp alacritty.yml ~/.config/alacritty/alacritty.yml
-
-cp zathurarc ~/.config/zathura/zathurarc
 
 cp gitconfig ~/.gitconfig
 
