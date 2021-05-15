6ce767106c36b8c2b8c67238f8d66cf4a29af7a6
Yan Mingzhi <realraindust@gmail.com>
2021-05-15 09:02:27 +0000

    update settings

--- config.fish
+++ config.fish
@@ -1,7 +1,4 @@
 source ~/.config/fish/fish/envrc.fish
 source ~/.config/fish/fish/aliases.fish
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/yanmingzhi/gcloud/google-cloud-sdk/path.fish.inc' ]; . '/Users/yanmingzhi/gcloud/google-cloud-sdk/path.fish.inc'; end
-
 starship init fish | source
