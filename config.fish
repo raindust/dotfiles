fa0563ed934668c5e2d508c6ecb8722769d1c1ef
Yan Mingzhi <realraindust@gmail.com>
2021-10-17 13:36:50 +0800

    remove alias

--- config.fish
+++ config.fish
@@ -1,5 +1,4 @@
 source ~/.config/fish/fish/envrc.fish
-source ~/.config/fish/fish/aliases.fish
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/yanmingzhi/gcloud/google-cloud-sdk/path.fish.inc' ]; . '/Users/yanmingzhi/gcloud/google-cloud-sdk/path.fish.inc'; end
