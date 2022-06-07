6b681649b9e672af036a522fadba64455ceeea53
Yan Mingzhi <realraindust@gmail.com>
2022-06-07 10:11:32 +0000

    set fish as default shell

--- config.fish
+++ config.fish
@@ -1,6 +1,3 @@
 source ~/.config/fish/fish/envrc.fish
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/yanmingzhi/gcloud/google-cloud-sdk/path.fish.inc' ]; . '/Users/yanmingzhi/gcloud/google-cloud-sdk/path.fish.inc'; end
-
 starship init fish | source
