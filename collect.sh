140a29aced9461f0b51c89a13c48f0c8021505b1
Yan Mingzhi <realraindust@gmail.com>
2023-04-10 17:54:33 +0800

    simplify settings

--- collect.sh
+++ collect.sh
@@ -1,25 +1,15 @@
 #!/usr/local/bin/fish
 
 cp ~/.bash_profile bash_profile
-cp ~/.zshrc zshrc
 cp ~/.envrc envrc
 cp ~/.aliases aliases
-cp ~/.oh-my-zsh/oh-my-zsh.sh oh-my-zsh.sh
 cp ~/.config/fish/config.fish config.fish
 cp -r ~/.config/fish/fish .
 
-cp ~/.config/nvim/init.vim init.vim
-cp ~/.ideavimrc ideavimrc
 cp ~/Library/Application\ Support/Code/User/keybindings.json keybindings.json
 cp ~/Library/Application\ Support/Code/User/settings.json settings.json
-cp ~/Library/Application\ Support/XiEditor/preferences.xiconfig preferences.xiconfig
 
 cp ~/.tmux.conf tmux.conf
 cp ~/.config/alacritty/alacritty.yml alacritty.yml
 
-cp ~/.config/zathura/zathurarc zathurarc
-
 cp ~/.gitconfig gitconfig
-
-# rust
-cp ~/.cargo/config rust_config
--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,9 +1,5 @@
 # set alias
 alias ls="exa"
-#alias find="fd"
-#alias grep="rg"
-alias vi="nvim"
-alias vim="nvim"
 alias man2="tldr"
 alias icloud="cd $HOME/Library/Mobile\ Documents/com~apple~CloudDocs"
 alias hh=hstr
@@ -16,16 +12,3 @@ function ...   ; cd ../.. ; end
 function ....  ; cd ../../.. ; end
 function ..... ; cd ../../../.. ; end
 
-# Docker
-alias dps='docker ps --format "table {{.Names}}\t{{.Status}}"'
-alias dim='docker images --format "table {{.ID}}\t{{.Repository}}\t{{.Tag}}"'
-alias dims='docker images --format "table {{.Repository}}\t{{.Size}}"'
-
-# java
-alias setJDK7='set -xg JAVA_HOME (/usr/libexec/java_home -v 1.7)'
-alias setJDK8='set -xg JAVA_HOME (/usr/libexec/java_home -v 1.8)'
-alias setJDK11='set -xg JAVA_HOME (/usr/libexec/java_home -v 11)'
-alias setJDK13='set -xg JAVA_HOME (/usr/libexec/java_home -v 13)'
-
-# tools
-alias setTar='set -xg PATH /usr/local/opt/gnu-tar/libexec/gnubin $PATH'
