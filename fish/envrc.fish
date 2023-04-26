2d795ee31e46c8bd6efdcf947b439361c2a0f605
Yan Mingzhi <realraindust@gmail.com>
2023-04-26 21:21:51 +0800

    update homebrew proxy

--- fish/envrc.fish
+++ fish/envrc.fish
@@ -9,7 +9,10 @@ set -xg GOPROXY https://goproxy.io
 set -xg RUSTUP_HOME $HOME/.rustup
 set -xg RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
 set -xg ALL_PROXY http://localhost:3213
-set -xg HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
+set -xg JAVA_HOME (/usr/libexec/java_home)
+set -xg HOMEBREW_BREW_GIT_REMOTE "https://mirrors.ustc.edu.cn/brew.git"
+set -xg HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
+set -xg HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
 
 # PATH
 set -xg PATH $PATH $GOROOT/bin
