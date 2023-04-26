6595790da3103de49f6a6a9f929479c8dbb6b90d
Yan Mingzhi <realraindust@gmail.com>
2023-04-26 21:22:25 +0800

    init conda

--- config.fish
+++ config.fish
@@ -2,3 +2,11 @@ if [ -z $TMUX ]; source ~/.config/fish/fish/envrc.fish; end
 source ~/.config/fish/fish/aliases.fish
 
 starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /usr/local/Caskroom/miniconda/base/bin/conda
+    eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
