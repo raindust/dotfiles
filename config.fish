737d8fb3a391135c13723d4128901a66dec09950
Yan Mingzhi <realraindust@gmail.com>
2024-06-20 19:34:24 +0800

    feat: update for new mac

--- config.fish
+++ config.fish
@@ -1,13 +1,29 @@
-if [ -z $TMUX ]; source ~/.config/fish/fish/envrc.fish; end
+source ~/.config/fish/fish/envrc.fish
 source ~/.config/fish/fish/aliases.fish
 
 starship init fish | source
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /usr/local/Caskroom/miniconda/base/bin/conda
-    eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/miniconda3/bin" $PATH
+    end
 end
 # <<< conda initialize <<<
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# pnpm
+set -gx PNPM_HOME "/Users/yanmingzhi/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+fish_add_path -a /Users/yanmingzhi/.foundry/bin
+direnv hook fish | source
--- fish/envrc.fish
+++ fish/envrc.fish
@@ -6,11 +6,12 @@ set -xg LANG en_US.UTF-8
 # enviroment variables
 set -xg GOPATH $HOME/dev
 set -xg GOBIN $GOPATH/bin
-set -xg GOPROXY https://goproxy.io
+set -xg GOROOT /usr/local/go
+set -xg GOPROXY https://goproxy.io,direct
 set -xg RUSTUP_HOME $HOME/.rustup
-set -xg RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
+set -xg RUSTUP_DIST_SERVER https://rsproxy.cn
+set -xg RUSTUP_UPDATE_ROOT https://rsproxy.cn/rustup
 set -xg ALL_PROXY http://localhost:3213
-set -xg JAVA_HOME (/usr/libexec/java_home)
 set -xg HOMEBREW_BREW_GIT_REMOTE "https://mirrors.ustc.edu.cn/brew.git"
 set -xg HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
 set -xg HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
@@ -19,6 +20,7 @@ set -xg HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
 set -xg PATH $PATH $GOROOT/bin
 set -xg PATH $PATH $GOBIN
 set -xg PATH $PATH $HOME/.cargo/bin
+set -xg PATH $PATH $HOME/.local/share/solana/install/active_release/bin
 
 # Other
 set -xg DISABLE_AUTO_TITLE "true"
